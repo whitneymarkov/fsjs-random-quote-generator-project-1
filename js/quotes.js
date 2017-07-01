e4fab377a9842a5843bbe8d8695e4d0ae7365485
Whitney <whitneymarkov@outlook.com>
2017-07-01 18:00:28 +0200

    added JavaScript, styling, and favicon

---
+++ js/quotes.js
@@ -0,0 +1,48 @@
+// array to hold quote, sources citation and year for each quote
+const quotes = [
+    {
+        quote: 'There is a way out of every box, a solution to every puzzle; it\'s just a matter of finding it.',
+        source: 'Nick Sagan',
+        citation: 'Attached',
+        year: '1993'
+    },
+    {
+        quote: 'Strong, deeply rooted desire is the starting point of all achievement.',
+        source: 'Napoleon Hill',
+        citation: 'The Law of Success',
+        year: '1937'
+    },
+    {
+        quote: 'Ce que l\'on conçoit bien s\'énonce clairement, Et les mots pour le dire arrivent aisément',
+        source: 'Nicolas Boileau',
+        citation: 'The Art of Poetry',
+        year: '1674'
+    },
+    {
+        quote: 'I\'m a great believer in luck, and I find the harder I work the more I have of it.',
+        source: 'Coleman Cox',
+        citation: 'Listen to This',
+        year: '1922'
+    },
+    {
+        quote: 'A little more persistence, a little more effort, and what seemed hopeless failure may turn to glorious success.',
+        source: 'Elbert Hubbard',
+        citation: 'Electrical Review',
+        year: '1895'
+    },
+    {
+        quote: 'Tea, Earl Grey, hot.',
+        source: 'Jean-Luc Picard',
+        citation: 'Star Trek TNG'
+    },
+    {
+        quote: 'Computers make excellent and efficient servants, but I have no wish to serve under them.',
+        source: 'Spock',
+        citation: 'Star Trek TOS'
+    },
+    {
+        quote: 'You\'ve got a lot to learn if you want people to think of you as a miracle worker.',
+        source: 'Scotty',
+        citation: 'Star Trek TNG'
+    }
+]
--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//selects a random quote object from the quotes array
+// returns the randomly selected quote objects
+function getRandomQuote() {
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+    let randomQuote = quotes[randomNumber];
+    return randomQuote;
+};
 
+//selects a random hex number up to 256
+function randomRGB() {
+    let color = Math.floor(Math.random() * 256);
+    return color;
+};
+
+function printQuote() {
+    //random background colour
+    let red = randomRGB();
+    let green = randomRGB();
+    let blue = randomRGB();
+    const rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+    // calls the getRandomQuote function
+    // constructs a string using the different properties of the quote object
+    let print = getRandomQuote();
+    let quote = '<p class="quote">' + print.quote + '</p>';
+    quote +='<p class="source">' + print.source;
+    //doesn't add citation if missing
+    if ( print.citation ) {
+        quote += '<span class="citation">' + print.citation + '</span>';
+    } else {
+        quote += '';
+    }
+    //doesn't add year if missing
+    if ( print.year ) {
+        quote += '<span class="year">' + print.year + '</span>';
+    } else {
+        quote += '';
+    }
+    quote += '</p>';
+
+    // the new value for the background colour
+    document.body.style.backgroundColor = rgbColor;
+    // stores the returned quote object in a variable.
+    document.getElementById('quote-box').innerHTML = quote;
+};
+
+document.getElementById('loadQuote').addEventListener('click', printQuote, false);
